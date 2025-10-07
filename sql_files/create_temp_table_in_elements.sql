5b985c3b07d5f62bdbf56bb49a915f12f63de116
Devin <devin.smith.work@gmail.com>
2025-10-07 08:27:20 -0700

    adding collation to temp table; handling null DOIs in SV report

--- sql_files/create_temp_table_in_elements.sql
+++ sql_files/create_temp_table_in_elements.sql
@@ -1,10 +1,10 @@
 CREATE TABLE #osti_submitted (
     osti_id INT,
     elements_id INT,
-    doi VARCHAR(80),
-    eschol_id VARCHAR(80),
+    doi VARCHAR(80) COLLATE Latin1_General_CI_AS,
+    eschol_id VARCHAR(80) COLLATE Latin1_General_CI_AS,
     eschol_pr_modified_when DATETIME,
-    prf_filename VARCHAR(200),
+    prf_filename VARCHAR(200) COLLATE Latin1_General_CI_AS,
     prf_size BIGINT,
     media_response_code INT,
     media_id INT,
